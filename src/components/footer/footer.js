af265eadeb68727dc6609262737ab1aa9e390e7f
cladg92 <cladg92@gmail.com>
2022-07-28 17:00:48 +0200

    Fix footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,59 +1,62 @@
 import React from "react";
 import "./footer.css";
-import twitter from "./twitter.svg";
 import github from "./github.svg";
 import linkedin from "./linkedin.svg";
+import { Col, Row } from "react-bootstrap";
 
 function Footer() {
   return (
     <footer className="page-footer">
-      <p>
-        Clara Di Gregorio
-        <br />
-        Bayreuth, Germany
-      </p>
-      <p className="email">
-        <a className="mail-link" href="mailto:digregorioclr@gmail.com">
-          <i class="fa-solid fa-envelope fa-xl" />
-        </a>
-        <a
-          className="mail-link"
-          href="mailto:digregorioclr@gmail.com"
-          id="mail"
-        >
-          digregorioclr@gmail.com
-        </a>
-      </p>
-      <p>Find me on</p>
-      <div className="social-media">
-        <a
-          href="https://github.com/cladg92"
-          target="_blank"
-          rel="noreferrer"
-          className="link filter-white"
-          aria-hidden="true"
-        >
-          <img src={github} alt="github" width="30" height="30" />
-        </a>
-        <a
-          href="https://twitter.com/?lang=it"
-          target="_blank"
-          rel="noreferrer"
-          className="link filter-white"
-          aria-hidden="true"
-        >
-          <img src={twitter} alt="github" width="30" height="30" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/clara-di-gregorio-8082291a9/"
-          target="_blank"
-          rel="noreferrer"
-          className="link filter-white"
-          aria-hidden="true"
-        >
-          <img src={linkedin} alt="github" width="30" height="30" />
-        </a>
-      </div>
+      <Row className="justify-content-center">
+        <Col>
+          <div className="contact-info">
+            <p>
+              Clara Di Gregorio
+              <br />
+              Bayreuth, Germany
+            </p>
+            <p className="email">
+              <a
+                className="mail-link"
+                href="mailto:digregorioclr@gmail.com"
+                id="mail"
+              >
+                digregorioclr@gmail.com
+              </a>
+            </p>
+          </div>
+        </Col>
+        <Col>
+          <div className="social-media">
+            <p>Find me on</p>
+            <div className="social-media">
+              <a
+                href="https://github.com/cladg92"
+                target="_blank"
+                rel="noreferrer"
+                className="link filter-white"
+                aria-hidden="true"
+              >
+                <img src={github} alt="github" width="30" height="30" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/clara-di-gregorio-8082291a9/"
+                target="_blank"
+                rel="noreferrer"
+                className="link filter-white"
+                aria-hidden="true"
+              >
+                <img src={linkedin} alt="github" width="30" height="30" />
+              </a>
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <a href="#" className="mail-link">
+            Impressum
+          </a>
+        </Col>
+      </Row>
     </footer>
   );
 }
