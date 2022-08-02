0c890048aa51d724e2f34cbec0973e57be625a85
cladg92 <cladg92@gmail.com>
2022-08-02 11:57:49 +0200

    Update about description

--- src/components/about-view/about-view.js
+++ src/components/about-view/about-view.js
@@ -14,23 +14,54 @@ function AboutView() {
       <Container className="about">
         <Row xs={1} md={2}>
           <Col>
-            <p className="profile__about">
-              As a biologist-turned-web developer, I’ve always been passionate
-              about learning new things, searching for logical explanations
-              behind patterns, and finding practical solutions. Programming was
-              a surprising discovery for me when, as a researcher-to-be, I found
-              myself spending most of my time behind a computer screen,
-              compiling and cleaning databases and performing analyses. <br />
+            <p className="profile__about" id="profile__about_1">
+              I'm a Full-stack web developer from Italy, currently located in
+              Bavaria.
+              <br />
+              <br /> I have a Master degree in Environmental Biology and have
+              been working as a Research assistant in the academic field until
+              last year. Programming was a surprising discovery for me when, as
+              a researcher-to-be, I found myself spending most of my time behind
+              a computer screen, compiling and cleaning databases and performing
+              analyses. I progressively realised I enjoyed the coding part of my
+              job more and more, until last year I decided to "follow my guts"
+              and transition into tech.
+              <br />
+              <br /> As a biologist-turned-web developer, I’ve always been
+              passionate about learning new things, searching for logical
+              explanations behind patterns, and finding practical solutions.
+              <br />
               My background in biology has taught me valuable skills such as
               analytical thinking and problem-solving skills, as well as
-              interpersonal and work-management skills. <br />
-              My curiosity and fascination with programming languages and their
-              potential to tackle problems and build solutions has eventually
-              led me to my current path.
+              interpersonal and work-management skills.
             </p>
           </Col>
           <Col>
-            <p className="profile__about"></p>
+            <p className="profile__about" id="profile__about_2">
+              My curiosity and fascination with programming languages and their
+              potential to tackle problems and build solutions has eventually
+              led me to my current path.
+              <br /> I see myself as a lifelong learner and that's just yet
+              another thing that makes web development so exciting, it's an
+              endless journey of learning! <br />
+              <br />
+              Apart from coding I enjoy hiking in the mountains of my home{" "}
+              <a
+                href="https://en.wikipedia.org/wiki/Abruzzo"
+                target="_blank"
+                rel="noreferrer"
+              >
+                region
+              </a>
+              , reading novels and traveling. <br />
+              Learning languages holds a special place among my hobbies and I
+              like to think this is somehow related to my inclination for
+              programming languages as well. I am fascinated by the cognitive
+              process involved in learning a new language, I enjoy noticing and
+              grasping its grammatical and lexical peculiarities, and, of
+              course, I strive for the end result: being able to communicate and
+              therefore gain a unique access to the culture behind it.
+            </p>
           </Col>
         </Row>
       </Container>
